d2208284982e329125fa71331b5f7342c6a28a52
Ilya Maltsev <ilya.maltsev@reltio.com>
2018-11-02 13:43:27 +0500

    First round completed

--- pairing.js
+++ pairing.js
@@ -1,1 +1,1 @@
-document.write ('<h3>Pairings:</h3><a id="rd-btn" class="btn btn-primary btn-sm" href="pairs1.html"> 1 </a> ')
+document.write ('<h3>Pairings:</h3><a id="rd-btn" class="btn btn-primary btn-sm" href="pairs1.html"> 1 </a> <a id="rd-btn" class="btn btn-primary btn-sm" href="pairs2.html"> 2 </a> ')
